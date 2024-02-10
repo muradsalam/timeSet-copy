3bde548ab450ed2521dadb1ea0cccf9c462d1c54
murad <murad@gmail.com>
2024-02-10 13:52:24 +0100

    all

---
+++ scr.js
@@ -0,0 +1,127 @@
+function uc() {
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '40';
+    }, 1000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '39';
+    }, 2000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '38';
+    }, 3000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '37';
+    }, 4000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '36';
+    }, 5000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '35';
+    }, 6000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '34';
+    }, 7000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '33';
+    }, 8000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '32';
+    }, 9000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '31';
+    }, 10000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '30';
+    }, 11000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '29';
+    }, 12000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '28';
+    }, 13000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '27';
+    }, 14000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '26';
+    }, 15000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '25';
+    }, 16000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '24';
+    }, 17000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '23';
+    }, 18000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '22';
+    }, 19000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '21';
+    }, 20000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '20';
+    }, 21000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '19';
+    }, 22000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '18';
+    }, 23000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '17';
+    }, 24000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '16';
+    }, 25000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '15';
+    }, 26000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '14';
+    }, 27000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '13';
+    }, 28000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '12';
+    }, 29000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '11';
+    }, 30000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '10';
+    }, 31000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '9';
+    }, 32000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '8';
+    }, 33000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '7';
+    }, 34000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '6';
+    }, 35000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '5';
+    }, 36000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '4';
+    }, 37000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '3';
+    }, 38000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '2';
+    }, 39000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = '1';
+    }, 40000);
+    setTimeout(() => {
+        document.getElementById('murad').innerHTML = 'AD GUNUN MUBAREK!!!';
+        document.getElementById('ayaz').innerHTML = 'BURA BAS!!!';
+    }, 41000);
+}
+uc()
